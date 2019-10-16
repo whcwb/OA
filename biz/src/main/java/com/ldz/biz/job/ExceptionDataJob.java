d79f8923f42ea4111ab1ea6ad978cc4a925296e7
libinbin <317330942@qq.com>
2019-10-16 14:55:04 +0800

    更新job日志记录

--- biz/src/main/java/com/ldz/biz/job/ExceptionDataJob.java
+++ biz/src/main/java/com/ldz/biz/job/ExceptionDataJob.java
@@ -2,6 +2,7 @@ package com.ldz.biz.job;
 
 import java.util.List;
 
+import org.joda.time.DateTime;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -12,7 +13,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ldz.biz.model.BizExceptionConfig;
+import com.ldz.biz.model.BizJoblog;
 import com.ldz.biz.service.BizExceptionService;
+import com.ldz.biz.service.BizJoblogService;
 import com.ldz.util.bean.ApiResponse;
 
 /*
@@ -27,18 +30,27 @@ public class ExceptionDataJob implements Job {
 	Logger errorLog = LoggerFactory.getLogger("error_info");
 	@Autowired
 	private BizExceptionService exceptionService;
+	@Autowired
+	private BizJoblogService jobLogService;
 	
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		try {
-			System.out.println("开启计划任务");
 			ApiResponse<List<BizExceptionConfig>> exps = exceptionService.getAllConfig();
 			if (exps.getCode() == ApiResponse.SUCCESS && exps.getResult().size() > 0){
 				List<BizExceptionConfig> configs = exps.getResult();
 				for (int i=0; i<configs.size(); i++){
 					BizExceptionConfig config = configs.get(i);
 					if (config.getDays() != null){
+						long startTime = System.currentTimeMillis();
 						exceptionService.expJobSave(config);
+						long endTime = System.currentTimeMillis();
+						//写日志记录
+						BizJoblog jobLog = new BizJoblog();
+						jobLog.setJob(config.getBz());
+						jobLog.setCjsj(DateTime.now().toString("yyyy-MM-dd HH:mm:ss"));
+						jobLog.setYxsc((endTime - startTime) / 1000);
+						jobLogService.save(jobLog);
 					}
 				}
 			}
